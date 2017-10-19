b5b6726c4ccfa3c2cb121c6e0f0c79c99088699f
Pedro Emannuel Skaaf <pedro.emanuel@pedros-macbook-pro.local>
2017-10-19 17:04:15 -0200

    Initial Commit

---
+++ TocaDeObjetos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TocaDeObjetos
+//
+//  Created by Pedro Emanuel on 19/10/17.
+//  Copyright © 2017 Pedro Emanuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
